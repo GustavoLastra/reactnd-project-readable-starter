644f78bf5cf6d11a2beca48b7977ce49d467d967
Gustavo Antonio Lastra Colorado <gustavo.lastrac@gmail.com>
2017-11-21 16:50:10 +0100

    add reactstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,50 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { slide as Menu } from 'react-burger-menu'
+import classNames from 'classnames';
+import { Button } from 'reactstrap';
 import './App.css';
-
-
-
-
 
 class App extends Component {
 
-  state = {
-    hamburgerOpen: false,
+  constructor (props) {
+    super(props);
+    this.state = {
+      open: false
+    };
   }
 
-
-  handleClick= () => {
-      this.setState({
-          hamburgerOpen: !this.state.open
-      });
+  handleClick() {
+	   this.setState({
+		     open: !this.state.open
+	      });
   }
+  showSettings (event) {
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div className="App">
+      <Menu
+      id={ "sidebar" }
+      className={ "my-menu" }
+      isOpen={this.state.open}
+      >
+        <ul>
+          <li><a id="home" className="menu-item" href="/">Home</a></li>
+          <li><a id="about" className="menu-item" href="/about">About</a></li>
+          <li><a id="contact" className="menu-item" href="/contact">Contact</a></li>
+          <li><a onClick={ this.showSettings } className="menu-item" href="">Settings</a></li>
+        </ul>
+      </Menu>
+
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-        <HamburgerMenu
-            isOpen={this.state.open}
-            menuClicked={this.handleClick.bind(this)}
-            width={18}
-            height={15}
-            strokeWidth={1}
-            rotate={0}
-            color='black'
-            borderRadius={0}
-            animationDuration={0.5}
-        />
+
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
