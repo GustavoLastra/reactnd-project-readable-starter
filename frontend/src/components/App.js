873e578f9d60259fa6d78cab9b7b90f2ef24b94d
Gustavo Antonio Lastra Colorado <gustavo.lastrac@gmail.com>
2017-11-25 17:01:28 +0100

    add ListCategories component

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -7,6 +7,8 @@ import classNames from 'classnames';
 import { Jumbotron, Container, Button,  Modal, ModalHeader, ModalBody, ModalFooter  } from 'reactstrap';
 import '../App.css';
 import ConnectedListPosts from './ListPosts';
+import ConnectedListCategories from './ListCategories';
+
 import { Link } from 'react-router-dom'
 
 class App extends Component {
@@ -19,35 +21,24 @@ class App extends Component {
   }
 
 
-
-  componentDidMount() {
-    const { categories, posts } =  this.props;
-    if ( Object.keys(categories).length === 0 ) {
-      this.props.getCategories();
-      //this.props.getPosts();
-    }
-  }
-
   render() {
     const {categories} =  this.props;
     return (
       <div className="App">
-      <Menu
-      id={ "sidebar" }
-      className={ "my-menu" }
-      isOpen={this.state.open}
-      >
-        <ul className="menu-list">
-          <li key="sortBy" className="menu-segment">Sort by</li>
-          <li key="hot"><a id="hot" className="menu-item" href="/">HOT</a></li>
-          <li key="fresh"><a id="fresh" className="menu-item" href="/about">FRESH</a></li>
-          <li key="categories" className="menu-segment">Categories</li>
-            {categories.map(category =>
-              <li key={category.name}><a id={category.name}  className="menu-item" href="/awesome">{category.name}</a></li>
-            )}
-        </ul>
-      </Menu>
 
+        <Menu
+          id={ "sidebar" }
+          className={ "my-menu" }
+          isOpen={this.state.open}
+        >
+          <ul className="menu-list">
+            <li key="sortBy" className="menu-segment">Sort by</li>
+            <li key="hot"><a id="hot" className="menu-item" href="/">HOT</a></li>
+            <li key="fresh"><a id="fresh" className="menu-item" href="/about">FRESH</a></li>
+            <li key="categories" className="menu-segment">Categories</li>
+              <ConnectedListCategories/>
+          </ul>
+        </Menu>
         <header className="App-header">
           <img src={taco} className="App-logo" alt="logo" />
           <h1 className="App-title">LordTaco</h1>
---
+++ frontend/src/components/ListCategories.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import {asyncGetCategories,asyncGetPosts} from '../actions'
+import classNames from 'classnames';
+import Post from './Post';
+import '../App.css';
+import { Link } from 'react-router-dom'
+
+class ListCategories extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  componentDidMount() {
+    const { categories } =  this.props;
+    if ( Object.keys(categories).length === 0 ) {
+      this.props.getCategories();
+      //this.props.getPosts();
+    }
+  }
+
+  render() {
+    const { categories} =  this.props;
+    return (
+      <div>
+        {categories.map(category =>
+          <li key={category.name}><a id={category.name}  className="menu-item" href="/awesome">{category.name}</a></li>
+        )}
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state){
+  return{
+    categories: state.categories,
+    posts: state.posts,
+    //categoriesIds: state.categoriesIds
+  }
+}
+
+function mapDispatchToProps(dispatch){
+  return{
+    getCategories: asyncGetCategories(dispatch),
+    getPosts: asyncGetPosts(dispatch),
+  }
+}
+
+const ConnectedListCategories =  connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ListCategories);
+
+export default ConnectedListCategories
